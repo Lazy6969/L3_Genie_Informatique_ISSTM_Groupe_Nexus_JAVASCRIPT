7b0d74f15512de2128d8773c492a216030b78f2e
Heather4-cpu <dolinejam4@gmail.com>
2026-04-17 23:46:52 +0300

    feat: setup main.jsx StrictMode + lazy loading

--- starters/react-cineapp/src/main.jsx
+++ starters/react-cineapp/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+/*import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,4 +7,29 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+)*/
+import { StrictMode, lazy, Suspense } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+const App = lazy(() => import('./App.jsx'))
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Suspense fallback={
+      <div style={{ 
+        color: 'white', 
+        background: '#141414', 
+        height: '100vh', 
+        display: 'flex', 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        fontSize: '20px'
+      }}>
+        🎬 Chargement CineMaster...
+      </div>
+    }>
+      <App />
+    </Suspense>
+  </StrictMode>
 )
